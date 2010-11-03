32821e8f68c29fc968a5e8f5d724474aaf01c78d
ja.abell <ja.abell@2832ba49-59ed-7d55-ca90-c7cf09fe64fe>
2010-11-03 23:27:01 +0000

    * Assigns workin.  - Materials GUI widgets in place but partial brains.  - Must include write assigns output.

--- trunk/src/gmsh2opensees.py
+++ trunk/src/gmsh2opensees.py
@@ -7,6 +7,8 @@ Created on Sat Oct 23 07:53:48 2010
 from libtabs import *
 from tkFileDialog import askopenfilename
 from tkFileDialog import asksaveasfilename
+import tkMessageBox as Mes
+
 from libgmsh2opensees import *
 
 class App:
@@ -15,27 +17,66 @@ class App:
         #Title
         # ----------------------------------------------------------------------
                
-        self.topMenu = Frame(self.master)
         
-        self.title = Label(self.topMenu)
-        self.title.text = 'gmsh2opensees'
-        self.title.fill = 'x'
-        self.title.pack()
+        #Create Menubar
+        #self.win = Toplevel(self)
+        #self.win = Toplevel(self.master)
+        #self.topMenu = Menu(self.win)
+        self.topMenu = Menu(self.master)
+        self.master['menu'] = self.topMenu
+        #self['menu'] = self.topMenu
         
-        self.loadGmshMeshButt = Button(self.topMenu, command = self.loadGmshFile)
-        self.loadGmshMeshButt['text'] = 'Load .msh file'
-        self.loadGmshMeshButt.pack(side = 'left', expand = 1)
+        self.menu_file = Menu(self.topMenu)
+        self.menu_edit = Menu(self.topMenu)
+        self.menu_about = Menu(self.topMenu)
+        self.menu_run = Menu(self.topMenu)
+        self.topMenu.add_cascade(menu = self.menu_file, label = 'File')
+        self.topMenu.add_cascade(menu = self.menu_edit, label = 'Edit')
+        self.topMenu.add_cascade(menu = self.menu_run, label = 'Run')
+        self.topMenu.add_cascade(menu = self.menu_about, label = '?')
         
-        self.loadProjectButt = Button(self.topMenu, command = self.loadProject)
-        self.loadProjectButt['text'] = 'Load Project'
-        self.loadProjectButt.pack(side = 'left', expand = 1)
         
-        self.saveProjectButt = Button(self.topMenu, command = self.saveProject)
-        self.saveProjectButt['text'] = 'Save Project'
-        self.saveProjectButt['state'] = 'disabled'
-        self.saveProjectButt.pack(side = 'left', expand = 1)
+        # FILE MENU
+        # Index
+        # 0: New Project
+        # 1: Load .gmsh file
+        # 2: Load Project...
+        # 3: Save Project
+        # 4: ---------- SEPARATOR-----------
+        # 5: Save Geometry
+        # 6: Save Materials
+        # 7: Save Assigns
+        # 8: Save Patterns
+        # 9: Save Recorders
+        # 10: Save Analysis
+        # 11: Save All files
         
-        self.topMenu.pack(expand = 1)
+        self.menu_file.add_command(label = 'New Project', command = self.clearProject)
+        self.menu_file.add_command(label = 'Load .gmsh file...', command = self.loadGmshFile)
+        self.menu_file.add_command(label = 'Load Project...', command = self.loadProject)
+        self.menu_file.add_command(label = 'Save Project', command = self.saveProject, state = 'disabled')
+        
+        self.menu_file.add_separator()
+        
+        self.menu_file.add_command(label = 'Save Geometry', command = self.saveGeom, state = 'disabled')
+        self.menu_file.add_command(label = 'Save Materials', command = self.saveMat, state = 'disabled')
+        self.menu_file.add_command(label = 'Save Assigns', command = self.saveAssigns, state = 'disabled')
+        self.menu_file.add_command(label = 'Save Patterns', command = self.savePat, state = 'disabled')
+        self.menu_file.add_command(label = 'Save Recorders', command = self.saveRec, state = 'disabled')
+        self.menu_file.add_command(label = 'Save Analysis', command = self.saveAnalysis, state = 'disabled')
+        self.menu_file.add_command(label = 'Save All Files', command = self.saveAll, state = 'disabled')
+        
+        # EDIT MENU
+        self.editProjectFilesButt = self.menu_edit.add_command(label = 'Project Files', command = self.editProjectFiles)
+        self.menu_edit.add_command(label = 'Preferences', command = self.editPreferences)
+        
+        # RUN MENU
+        self.runOpenSEESButt = self.menu_run.add_command(label = 'Run in OpenSEES', command = self.runOpenSEES)
+        self.getResultsButt = self.menu_run.add_command(label = 'Get Results', command = self.getResults)
+        
+        # About MENU
+        self.menu_about.add_command(label = 'Help', command = self.showHelp)
+        self.menu_about.add_command(label = 'About...', command = self.showAbout)
         
         # ----------------------------------------------------------------------
         # Tabs setup
@@ -46,43 +87,39 @@ class App:
         self.tabMaster.add(self.tabSummary, text='Model Summary', state='disabled')
         self.tabSummary.createWidgets()
         
+        self.tabMaterials = FrameMaterials(self.tabMaster)
+        self.tabMaster.add(self.tabMaterials, text='Materials', state='disabled')
+        self.tabMaterials.createWidgets()
         
         self.tabAssign = FrameAssign(self.tabMaster)
         self.tabMaster.add(self.tabAssign, text='Assignments', state='disabled')
         self.tabAssign.createWidgets()
         
+        self.tabPatterns = FramePatterns(self.tabMaster)
+        self.tabMaster.add(self.tabPatterns, text='Patterns', state='disabled')
+        self.tabPatterns.createWidgets()
+        
         self.tabRecord = FrameRecorders(self.tabMaster)
         self.tabMaster.add(self.tabRecord, text='Recorders', state='disabled')
-        
+        self.tabRecord.createWidgets()
+
         self.tabAnalysis = FrameAnalysis(self.tabMaster)
         self.tabMaster.add(self.tabAnalysis, text='Analysis Setup', state='disabled')
+        self.tabAnalysis.createWidgets()
         
-            #        self.tabMaster['width'] = 800
-            #        self.tabMaster['height'] = 600
+        self.tabVariables = FrameVariables(self.tabMaster)
+        self.tabMaster.add(self.tabVariables, text='Variables', state='disabled')
+        self.tabVariables.createWidgets()
+
+
         self.tabMaster.pack()
         
         # ----------------------------------------------------------------------
-        # Lower Buttons
+        # Lower Save Buttons
         # ----------------------------------------------------------------------
         
-        self.buttonFrame = Frame(self.master)
-        
-        self.saveGeomButt = Button(self.buttonFrame, command=self.saveGeom)
-        self.saveGeomButt['text'] = 'Save Geometry'
-        self.saveGeomButt['state'] = 'disabled'
-        self.saveGeomButt.pack(side = 'left', expand = 1)
-        
-        self.saveAssignButt = Button(self.buttonFrame, command=self.saveAssigns)
-        self.saveAssignButt['text'] = 'Save Physical Assigns'
-        self.saveAssignButt['state'] = 'disabled'
-        self.saveAssignButt.pack(side = 'left', expand = 1)
-        
-        self.saveAnalysisButt = Button(self.buttonFrame, command=self.saveAnalysis)
-        self.saveAnalysisButt['text'] = 'Save Analysis Options'
-        self.saveAnalysisButt['state'] = 'disabled'
-        self.saveAnalysisButt.pack(side = 'left', expand = 1)
-        
-        self.buttonFrame.pack(expand = 1)
+        #self.buttonFrame = Frame(self.master)
+        #self.buttonFrame.pack(expand = 1)
         
     def loadGmshFile(self):
         self.fname = askopenfilename(parent=self.master, title="gmsh2OpenSEES:  Seleccionar malla de origen", initialdir=".", filetypes=[("GMSH mesh","*.msh")])
@@ -90,53 +127,196 @@ class App:
         
         if self.model.success == 1:
             print 'Success in loading GMSH file!'
-            #self.tabMaster.tab(self.tabSummary, state ="normal")
-            self.tabMaster.tab(0,state="normal")
-            self.tabMaster.tab(1,state="normal")
-            self.tabMaster.tab(2,state="normal")
-            self.tabMaster.tab(3,state="normal")
-            self.tabMaster.select(0)
-            self.saveGeomButt['state'] = "normal"
-            
-            self.tabSummary.updateWidgets(self.model)
-            self.tabAssign.updateWidgets(self.model)
-            self.saveProjectButt['state'] = 'normal'
+            self.activateGui()
+            self.model.subscribe(self)
+          
+              
         else:
             pass
         
     def saveGeom(self):
-        self.outfname = asksaveasfilename(parent=self.master,title="gmsh2OpenSEES: Seleccionar el archivo de destino", filetypes=[("OpenSEES TCL Script","*.ops")], initialfile=self.fname[:-4]+'.ops')
-        self.model.writeModelToOpsFile(self.outfname)
-        
+        self.outfname = asksaveasfilename(parent=self.master,title="gmsh2OpenSEES: Seleccionar el archivo de destino", filetypes=[("OpenSEES TCL Script","*.ops")], initialfile=self.model.fnames['geometry'])
+        self.model.fnames['geometry'] = self.outfname
+        self.model.writeGeometry()
         pass
+    
+    def saveMat(self):
+        print 'saveMat'
+        pass
+
     def saveAssigns(self):
         print 'saveAssigns'
         pass
+        
+    def savePat(self):
+        print 'savePat'
+        pass
+        
+    def saveRec(self):
+        print 'saveRec'
+        pass
+        
     def saveAnalysis(self):
         print 'saveAnalysis'
         pass
+        
+    def saveAll(self):
+        print 'saveRec'
+        pass
+        
     def loadProject(self):
         print 'loadProject'
         self.fname = askopenfilename(parent=self.master, title="gmsh2OpenSEES:  Seleccionar el archivo de destino", initialdir=".", filetypes=[("gmsh2opensees Binary Model Representation","*.bmr")])
         self.model = Model.loadDatabase(self.fname)
         print 'Success in loading GMSH file!'
-        #self.tabMaster.tab(self.tabSummary, state ="normal")
+        
+        if self.model.success == 1:
+            self.activateGui()
+            self.model.subscribe(self)
+            self.catchModelChange()
+        
+        pass
+        
+    def activateGui(self):
         self.tabMaster.tab(0,state="normal")
         self.tabMaster.tab(1,state="normal")
         self.tabMaster.tab(2,state="normal")
         self.tabMaster.tab(3,state="normal")
+        self.tabMaster.tab(4,state="normal")
+        self.tabMaster.tab(5,state="normal")
+        self.tabMaster.tab(6,state="normal")
         self.tabMaster.select(0)
-        self.saveGeomButt['state'] = "normal"
+        self.menu_file.entryconfig(3,state = 'normal')
+        self.menu_file.entryconfig(5,state = 'normal')
+        #self.saveGeomButt['state'] = "normal"
         
         self.tabSummary.updateWidgets(self.model)
         self.tabAssign.updateWidgets(self.model)
-        self.saveProjectButt['state'] = 'normal'
-        
-        pass
+        self.tabMaterials.updateWidgets(self.model)
+        #self.saveProjectButt['state'] = 'normal'
+        #self.dumpModelButt['state'] = 'normal'
+            
     def saveProject(self):
         fname = asksaveasfilename(parent=self.master,title="gmsh2OpenSEES: Seleccionar el archivo de destino", filetypes=[("gmsh2opensees Binary Model Representation","*.bmr")], initialfile=self.fname[:-4]+'.bmr')
         self.model.saveDatabase(fname)
         print 'saveProject'
+        pass
+        
+    def dumpModelToIntepreter(self):
+        ans = Mes.askyesno(message='This will end current session', icon='warning', title='gmsh2OpenSEES: Dump to python interpreter.')
+
+        if ans == TRUE:
+            return self.model
+            self.destroy()
+        else:
+            pass
+    
+    def checkModelFlags(self):
+        # FILE MENU
+        # Index
+        # 0: New Project
+        # 1: Load .gmsh file
+        # 2: Load Project...
+        # 3: Save Project
+        # 4: ---------- SEPARATOR-----------
+        # 5: Save Geometry
+        # 6: Save Materials
+        # 7: Save Assigns
+        # 8: Save Patterns
+        # 9: Save Recorders
+        # 10: Save Analysis
+        # 11: Save All files
+        if self.model.flags['geometry'] == 1:
+            self.menu_file.entryconfig(5,state='normal')
+            #self.saveGeomButt['state'] = 'normal'
+        else:
+            self.menu_file.entryconfig(5,state='disabled')
+            #self.saveGeomButt['state'] = 'disabled'
+            pass
+            
+        if self.model.flags['master'] == 1:
+            #self.saveMasterButt['state'] = 'normal'
+            pass
+        else:
+            #self.saveMasterButt['state'] = 'disabled'
+            pass
+            
+        if self.model.flags['materials'] == 1:
+            self.menu_file.entryconfig(6,state='normal')
+            #self.saveMatButt['state'] = 'normal'
+        else:
+            self.menu_file.entryconfig(6,state='disabled')
+            #self.saveMatButt['state'] = 'disabled'
+            pass
+            
+        if self.model.flags['assigns'] == 1:
+            self.menu_file.entryconfig(7,state='normal')
+            #self.saveAssignButt['state'] = 'normal'
+        else:
+            self.menu_file.entryconfig(7,state='disabled')
+            #self.saveAssignButt['state'] = 'disabled'
+            pass
+            
+        if self.model.flags['patterns'] == 1:
+            self.menu_file.entryconfig(8,state='normal')
+            #self.savePatButt['state'] = 'normal'
+        else:
+            self.menu_file.entryconfig(8,state='disabled')
+            #self.savePatButt['state'] = 'disabled'
+            pass
+            
+        if self.model.flags['recorders'] == 1:
+            self.menu_file.entryconfig(9,state='normal')
+            #self.saveRecButt['state'] = 'normal'
+        else:
+            self.menu_file.entryconfig(9,state='disabled')
+            #self.saveRecButt['state'] = 'disabled'
+            pass
+            
+        if self.model.flags['analysis'] == 1:
+            self.menu_file.entryconfig(10,state='normal')
+            #self.saveAnalysisButt['state'] = 'normal'
+        else:
+            self.menu_file.entryconfig(10,state='disabled')
+            #self.saveAnalysisButt['state'] = 'disabled'
+            pass
+            
+        if self.model.flags['elements'] == 1:
+            #self.saveGeomButt['state'] = 'normal'
+            pass
+        else:
+            #self.saveGeomButt['state'] = 'disabled'
+            pass
+            
+    def clearProject(self):
+        pass
+        
+    def editProjectFiles(self):
+        pass
+        
+    def editPreferences(self):
+        pass
+        
+    def runOpenSEES(self):
+        pass
+        
+    def getResults(self):
+        pass
+        
+    def showAbout(self):
+        pass
+        
+    def showHelp(self):
+        pass
+        
+    def updateWidgets(self):
+        self.tabAssign.updateWidgets(self.model)
+        self.tabMaterials.updateWidgets(self.model)
+        pass
+    
+    def catchModelChange(self):
+        self.checkModelFlags()
+        self.updateWidgets()
         pass
     
     def __init__(self, master):
@@ -144,10 +324,11 @@ class App:
         self.master.title("gmsh2OpenSEES")
         self.model = 'void'
         self.createWidgets()
-
+        
 
 
 # MAIN
 root = Tk()
-a = App(root)
+root.option_add('*tearOff', FALSE)   #Remove undockable menus
+model = App(root)
 root.mainloop()
